f7af3f321d73064aa495d38b5dc541c84918808d
Paul Le Gall <darky.dev@outlook.com>
2024-11-29 22:34:20 +0100

    [ADD] Project baseline

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+
+
+const App = () => {
+    return (
+        <div className={styles.App}>
+            <h1>Hello RTC</h1>
+        </div>
+    )
+}
+
+export default App;
